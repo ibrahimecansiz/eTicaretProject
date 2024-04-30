0a4c51d7422d6fac4a8893eca4ce1d78f807edcf
ibrahimecansiz <ibrahim.ethem.cansiz@gmail.com>
2024-04-30 13:55:17 +0300

    first commit

---
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,29 @@
+﻿using ETicaretAPI.Application;
+using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Persistence.Concretes;
+using ETicaretAPI.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistanceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<ETicaretAPIDbContext>(opt => opt.UseNpgsql(Configuration.ConnectionString));
+
+            services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
+            services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
+            services.AddScoped<IOrderReadRepository, OrderReadRepository>();
+            services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
+            services.AddScoped<IProductReadRepository, ProductReadRepository>();
+            services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+        }
+    }
+}
