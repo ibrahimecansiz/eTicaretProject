3a1ac1dcb905a0b75558d487b42008d973de2d60
ibrahimecansiz <ibrahim.ethem.cansiz@gmail.com>
2024-05-06 16:56:35 +0300

    setup module

--- ETicaretClient/src/app/app.module.ts
+++ ETicaretClient/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LayoutModule } from './admin/layout/layout.module';
+import { AdminModule } from './admin/admin.module';
+import { UiModule } from './ui/ui.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AdminModule,
+    UiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
