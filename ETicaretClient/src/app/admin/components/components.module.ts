6414f1750f1b6b01cf0404086d9d0be0be4b1188
ibrahimecansiz <ibrahim.ethem.cansiz@gmail.com>
2024-05-08 12:03:34 +0300

    layout

--- ETicaretClient/src/app/admin/components/components.module.ts
+++ ETicaretClient/src/app/admin/components/components.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { CustomersModule } from './customers/customers.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    CustomersModule,
     ProductsModule,
     OrdersModule,
     DashboardModule
--- ETicaretClient/src/app/app.module.ts
+++ ETicaretClient/src/app/app.module.ts
@@ -2,18 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LayoutModule } from './admin/layout/layout.module';
 import { AdminModule } from './admin/admin.module';
 import { UiModule } from './ui/ui.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     AdminModule,
-    UiModule
+    UiModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
