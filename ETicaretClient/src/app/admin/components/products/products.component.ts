e4aed9ed02ac996c1559d40888d2e70a3336418d
ibrahimecansiz <ibrahim.ethem.cansiz@gmail.com>
2024-05-14 10:05:29 +0300

    Add Product Service

--- ETicaretClient/src/app/admin/components/products/products.component.ts
+++ ETicaretClient/src/app/admin/components/products/products.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
-import { Product } from 'src/app/contracts/product';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
@@ -18,28 +17,6 @@ export class ProductsComponent extends BaseComponent implements OnInit {
 
   }
   ngOnInit(): void {
-    this.showSpinner(SpinnerType.BallAtom);
-    this.httpClientService.get<Product[]>({
-      controller: "products"
-    }).subscribe(data => console.log(data));
-
-    // this.httpClientService.post({
-    //   controller: "products"
-    // },{
-    //   name: "Kalem",
-    //   stock: 100,
-    //   price: 15
-    // }).subscribe();
-    // this.httpClientService.put({
-    //   controller: "products"
-    // },{
-    //   id: "f1686fb3-c3f5-4c1f-856e-b3d2621bab17",
-    //   name: "Boyama Kitabı",
-    //   stock: 123,
-    //   price: 100
-    // }).subscribe();
-
-    // this.httpClientService.delete({controller: "products"}, "f1efd94e-72f2-4d81-9e9e-7a891bff7c8d").subscribe();
   }
 
 }
